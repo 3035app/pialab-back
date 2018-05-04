d35452e8fe6c3aced1df6906f03ee3398563eec2
Fanch <fanch.gerbes@gmail.com>
2018-05-04 18:14:17 +0200

    [CI] add set pgpass script to allow cnx from ci scripts

--- bin/ci-scripts/create_database.sh
+++ bin/ci-scripts/create_database.sh
@@ -8,7 +8,7 @@ then
 else
     echo "Please run this script from project root, and check .env file as it is mandatory"
     echo "If it is missing a quick solution is :"
-    echo "ln -s .env.travis .env"
+    echo "ln -s .env.dist .env"
     exit 42
 fi
 
