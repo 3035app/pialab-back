0ec6cddcc7a914c138cfdbd4685dafa8c464e9bd
Fanch <fanch.gerbes@gmail.com>
2018-05-07 14:07:23 +0200

    [CI] Add CI in Back Auth (#4)

---
+++ bin/ci-scripts/create_schema.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -ex
+
+# TODO share this between script (in an include)
+if [ -f .env ]
+then
+    source .env
+else
+    echo "Please run this script from project root, and check .env file as it is mandatory"
+    echo "If it is missing a quick solution is :"
+    echo "ln -s .env.dist .env"
+    exit 42
+fi
+
+bin/console doctrine:database:create --connection=default --if-not-exists --no-interaction 
+bin/console doctrine:database:create --connection=customer --if-not-exists --no-interaction 
+
+bin/console doctrine:schema:update --force --no-interaction
+bin/console doctrine:schema:validate  --no-interaction 
---
+++ bin/ci-scripts/set_pgpass.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -ex
+
+# TODO share this between script (in an include)
+if [ -f .env ]
+then
+    source .env
+else
+    echo "Please run this script from project root, and check .env file as it is mandatory"
+    echo "If it is missing a quick solution is :"
+    echo "ln -s .env.dist .env"
+    exit 42
+fi
+
+
+if [ -n "${DBHOST}" ]
+then
+    touch $HOME/.pgpass $HOME/pgpass.tmp
+    cp $HOME/.pgpass $HOME/pgpass.tmp
+    echo ${DBHOST}:5432:*:${DBROOTUSER}:${DBROOTPASSWORD} >> $HOME/pgpass.tmp
+    echo ${DBHOST}:5432:*:${DBOAUTHUSER}:${DBOAUTHPASSWORD} >> $HOME/pgpass.tmp
+    echo ${DBHOST}:5432:*:${DBCUSTOMERUSER}:${DBCUSTOMERPASSWORD} >> $HOME/pgpass.tmp
+    sort -u $HOME/pgpass.tmp > $HOME/.pgpass
+    chmod 600 $HOME/.pgpass
+    cat  $HOME/.pgpass
+fi
