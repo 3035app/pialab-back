6402cabc091fcb70a22b7584ba0575d6df3dec1e
PapsOu <vivien.frasca@libre-informatique.fr>
2018-05-24 17:59:24 +0200

    Add backend management interfaces

--- src/Entity/Pia/PiaTemplate.php
+++ src/Entity/Pia/PiaTemplate.php
@@ -42,7 +42,7 @@ class PiaTemplate implements Timestampable
     protected $name;
 
     /**
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", nullable=true)
      *
      * @var string
      */
@@ -63,13 +63,78 @@ class PiaTemplate implements Timestampable
      */
     protected $pias;
 
-    public function __construct()
+    public function __construct(string $name)
     {
+        $this->name = $name;
         $this->pias = new ArrayCollection();
     }
 
     public function isEnabled(): bool
     {
         return $this->enabled;
+    }
+
+    /**
+     * @return array
+     */
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param array $data
+     */
+    public function setData(array $data): void
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getPias(): Collection
+    {
+        return $this->pias;
+    }
+
+    /**
+     * @param Collection $pias
+     */
+    public function setPias(Collection $pias): void
+    {
+        $this->pias = $pias;
     }
 }
---
+++ src/Form/PiaTemplate/CreatePiaTemplateForm.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * Copyright (C) 2015-2018 Libre Informatique
+ *
+ * This file is licenced under the GNU LGPL v3.
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace PiaApi\Form\PiaTemplate;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+class CreatePiaTemplateForm extends AbstractType
+{
+    public function __construct(RegistryInterface $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'required' => true,
+                'label'    => 'Nom',
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'fluid',
+                ],
+                'label' => 'Créer la structure',
+            ])
+        ;
+    }
+}
