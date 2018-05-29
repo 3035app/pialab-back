bdcf150f538870723e28d326c8ab748b74a33df8
PapsOu <vivien.frasca@libre-informatique.fr>
2018-05-29 16:57:52 +0200

    Fix templates for structures

--- src/Repository/PiaTemplateRepository.php
+++ src/Repository/PiaTemplateRepository.php
@@ -15,23 +15,25 @@ use PiaApi\Entity\Pia\Structure;
 
 class PiaTemplateRepository extends EntityRepository
 {
-    public function findAvailablePiaTemplatesForStructure(Structure $structure): array
+    public function findAvailablePiaTemplatesForStructure(?Structure $structure): array
     {
         $qb = $this->createQueryBuilder('pt');
 
         $qb
             ->leftJoin('pt.structures', 'structures')
             ->leftJoin('pt.structureTypes', 'structureTypes')
-            ->set('pt.enabled', true)
-            ->where($qb->expr()->in('structures', ':structure'))
-            ->orWhere($qb->expr()->in('structureTypes', ':structureType'))
-            // ->andWhere($qb->expr()->in('pt.enabled', $qb->true))
-        ;
+            ->set('pt.enabled', true);
 
-        $qb->setParameters([
-            'structure'     => $structure,
-            'structureType' => $structure->getType(),
-        ]);
+        if ($structure !== null) {
+            $qb
+                ->where($qb->expr()->in('structures', ':structure'))
+                ->orWhere($qb->expr()->in('structureTypes', ':structureType'));
+
+            $qb->setParameters([
+                'structure'     => $structure,
+                'structureType' => $structure->getType(),
+            ]);
+        }
 
         return $qb->getQuery()->getResult();
     }
