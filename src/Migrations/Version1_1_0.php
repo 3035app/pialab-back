0362abd3c4866be43387388be5530263955a162c
Romain Sanchez <romainsancho@gmail.com>
2018-06-19 17:53:35 +0200

    Add type property to Pia model (#20)

--- src/Migrations/Version1_1_0.php
+++ src/Migrations/Version1_1_0.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 /*
  * Copyright (C) 2015-2018 Libre Informatique
  *
+<<<<<<< HEAD
+ * This file is licenced under the GNU LGPL v3.
+=======
  * This file is licensed under the GNU LGPL v3.
+>>>>>>> 3409f3559d9ea36a4d4201666110cafdcf3bd661
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
  */
@@ -17,6 +21,8 @@ use Doctrine\DBAL\Schema\Schema;
 use PiaApi\Migrations\Lib\MigrationTrait;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use PiaApi\Entity\Pia\UserProfile;
+use PiaApi\Entity\Oauth\User;
 
 class Version1_1_0 extends AbstractMigration implements ContainerAwareInterface
 {
@@ -27,6 +33,7 @@ class Version1_1_0 extends AbstractMigration implements ContainerAwareInterface
 
     private $migrations = [
         'schema' => [
+            '20180612141711',
         ],
         'data' => [
             '20180619143737',
@@ -36,6 +43,22 @@ class Version1_1_0 extends AbstractMigration implements ContainerAwareInterface
     // #########################################
     //         OLD VERSIONS BELOW
     // #########################################
+
+    protected function Version20180612141711_up(Schema $schema): void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE pia ADD type VARCHAR(255) NULL');
+        $this->addSql("UPDATE pia set type='advanced'");
+        $this->addSql('ALTER TABLE pia ALTER COLUMN type SET NOT NULL');
+    }
+
+    protected function Version20180612141711_down(Schema $schema): void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE pia DROP type');
+    }
 
     protected function Version20180619143737_up(Schema $schema): void
     {
