e65a9ed736394fb6112dd781535d01b484fcdd95
PapsOu <vivien.frasca@gmail.com>
2018-07-31 14:21:09 +0200

    Fix migration

--- src/Migrations/Version20180731100339.php
+++ src/Migrations/Version20180731100339.php
@@ -23,6 +23,8 @@ class Version20180731100339 extends AbstractMigration implements ContainerAwareI
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
+        $this->addSql('CREATE SEQUENCE pia_processing_id_seq INCREMENT BY 1 MINVALUE 1 START 1;');
+        $this->addSql('CREATE SEQUENCE pia_processing_data_type_id_seq INCREMENT BY 1 MINVALUE 1 START 1;');
         $this->addSql('CREATE TABLE pia_processing (id INT NOT NULL, folder_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, author VARCHAR(255) NOT NULL, description TEXT NOT NULL, life_cycle_description TEXT NOT NULL, data_medium_description TEXT NOT NULL, standards_description TEXT NOT NULL, processors TEXT NOT NULL, controllers TEXT NOT NULL, data_transfer_outside_eu TEXT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_81E5D0EC162CB942 ON pia_processing (folder_id)');
         $this->addSql('CREATE TABLE pia_processing_data_type (id INT NOT NULL, processing_id INT DEFAULT NULL, reference VARCHAR(255) NOT NULL, data TEXT NOT NULL, retention_period VARCHAR(255) NOT NULL, sensitive BOOLEAN NOT NULL, PRIMARY KEY(id))');
@@ -33,6 +35,10 @@ class Version20180731100339 extends AbstractMigration implements ContainerAwareI
         $this->addSql('ALTER TABLE pia ADD processing_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE pia ADD CONSTRAINT FK_253A30625BAE24E8 FOREIGN KEY (processing_id) REFERENCES pia_processing (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE INDEX IDX_253A30625BAE24E8 ON pia (processing_id)');
+
+        $this->addSql('ALTER TABLE pia DROP CONSTRAINT fk_253a3062162cb942;');
+        $this->addSql('DROP INDEX idx_253a3062162cb942;');
+        $this->addSql('ALTER TABLE pia DROP folder_id;');
     }
 
     public function down(Schema $schema)
@@ -45,5 +51,11 @@ class Version20180731100339 extends AbstractMigration implements ContainerAwareI
         $this->addSql('DROP TABLE pia_processing_data_type');
         $this->addSql('DROP INDEX IDX_253A30625BAE24E8');
         $this->addSql('ALTER TABLE pia DROP processing_id');
+        $this->addSql('DROP SEQUENCE pia_processing_id_seq');
+        $this->addSql('DROP SEQUENCE pia_processing_data_type_id_seq');
+
+        $this->addSql('ALTER TABLE pia ADD folder_id INT EFAULT NULL;');
+        $this->addSql('ALTER TABLE pia ADD CONSTRAINT fk_253a3062162cb942 FOREIGN KEY (folder_id) REFERENCES pia_folder (id) NOT DEFERRABLE INITIALLY IMMEDIATE;');
+        $this->addSql('CREATE INDEX idx_253a3062162cb942 ON pia (folder_id);');
     }
 }
