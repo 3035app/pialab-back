1f680547a7202c7cb53132d097ebb7526fc9109f
Fabrice <fabrice.petrequin@libre-informatique.fr>
2018-08-08 17:08:14 +0200

    Add data field to the data type creation method

--- tests/api/140_ProcessingDataTypeCest.php
+++ tests/api/140_ProcessingDataTypeCest.php
@@ -31,6 +31,7 @@ class ProcessingDataTypeCest
         'processing_id'     => null,
         'retention_period'  => 'period',
         'sensitive'         => true,
+        'data'              => ['data' => 'edited'],
     ];
 
     /**
