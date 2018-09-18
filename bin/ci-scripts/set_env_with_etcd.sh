c9f0a937bfe057e9e4bd49d7495715467de8656b
PapsOu <vivien.frasca@gmail.com>
2018-09-18 12:18:40 +0200

    Fix underscored key (replaced by slashes, thanks to etcd rule...)

--- bin/ci-scripts/set_env_with_etcd.sh
+++ bin/ci-scripts/set_env_with_etcd.sh
@@ -165,7 +165,7 @@ $ETCDCTLCMD put $Prefix/postgres/default/password $DatabasePassword $ETCDENDPOIN
 
 $ETCDCTLCMD put $Prefix/smtp/default/url $MailerUrl $ETCDENDPOINT
 $ETCDCTLCMD put $Prefix/smtp/default/sender $MailerSender $ETCDENDPOINT
-$ETCDCTLCMD put $Prefix/smtp/default/sender_name $MailerSenderName $ETCDENDPOINT
+$ETCDCTLCMD put $Prefix/smtp/default/sender/name $MailerSenderName $ETCDENDPOINT
 
 # set symfony env
 $ETCDCTLCMD put $Prefix/symfony/env $SYMFONYENV $ETCDENDPOINT
